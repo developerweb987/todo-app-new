f1e61cf40f7def3b1dfea2a0b1f7683debc59075
Muhammad Haris Khan <160781149+developerweb987@users.noreply.github.com>
2026-01-30 16:09:41 +0500

    next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://todo-app-new-backend.vercel.app',
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://todo-app-new-backend.vercel.app"
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
